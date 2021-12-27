16d4198b7b0b46c122e00fcbd00c9256a7bf60fe
Anand <73114463+Anand4756@users.noreply.github.com>
2021-12-27 19:00:46 +0530

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.animate', {
-  strings: ["The land of Heritage and Culture"],
+  strings: ["The land of Heritage and Culture","Home of oldest Hindu temple","Having Oldest University"],
   typeSpeed: 90,
   backSpeed: 60,
   loop: true
